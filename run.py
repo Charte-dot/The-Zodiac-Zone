bbb9d6f5d072e11d570aa7a0c9517fd1192304d0
Caroline Harte <caroline.harte@yahoo.com>
2022-04-16 21:57:29 +0000

    Add welcome message and date of birth calculator

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+from datetime import date
+
+print(r'______________________  ____  _____________________ ')
+print(r'|__  _| | | | __|___   /    \ |  \ | |   /\   | ___|')
+print(r' |  | | |_| | |_    / /| /\ | ||\ \| |  /  \  | |   ')
+print(r' |  | | ___ | __|  / / | || | || | | | / /\ \ | |   ')
+print(r' |  | | | | | |_  / /__| \/ | ||/ /| |/ ____ \| |__ ')
+print(r' |__| |_| |_|___|/_____\____/ |_ / |_/_/    \_\____|\n')
+
+
+print('Welcome to The Zodiac Zone\n')
+print('The place to find out you Zodiac sign\n')
+print('And get a personalized horoscope for your sign\n')
+print('Please enter you name to begin\n')
+name = input()
+print('hello ' + name)
+print('Please enter your Birthdate\n')
+
+Year = input("What's your year of birth? [ex: 1980]\n")
+Month = input("What's your month of birth? [ex: 10]\n")
+Day = input("What's your day of birth? [ex: 01]\n")
+
+print('Your Date of Birth is ', (Day + "/" + Month + "/" + Year))

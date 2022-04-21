5bc636da39e0344b631ff728c019d9c79325097d
Caroline Harte <caroline.harte@yahoo.com>
2022-04-21 21:37:36 +0000

    Add information strings for random choice to create personal horoscope

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from datetime import date
+import random
 import time
 
 print(r'***********************************************************')
@@ -21,10 +21,25 @@ name = input()
 print('Hello ' + name)
 print('Please enter your day and month of Birth\n')
 
-"""
-This function only excepts numbers from user input
-for date of birth
-"""
+first = [
+        "Today is perfect for new endeavors.",
+        "Today is the day to cherish and embrace others.",
+        "The tensions of this week will feel heavier today than yesterday.", 
+        "Making yourself useful is a main component of a successful day.",
+        "Today, exercise caution when crossing the street.\n"]
+
+second = [
+        "Remember that good things come to those who work hard. ",
+        "Dont let the circumstances bring you down."
+        "Patience is key, but sometimes a little push can get the job done.",
+        "A smile can get you a long way.\n"]
+
+third = [
+        "Looking ahead may seem like a waste, but it pays off in the end.",
+        "Luck favors those who mind the risks and take them. ",
+        "Today is the day for that thing you always wanted to do. ",
+        "Luck is on your side today, so seize it! ",
+        "Things are looking up for you!\n"]
 
 
 def month(int):
@@ -81,45 +96,68 @@ print('Thank you! I will now consult the cosmos and return your Zodiac sign\n')
 Depending on user input, if/elif statement will calculate which
 sign to display after user inputs date of birth
 """
-
+# returns Aquarius if user input is within month and day parameters
 if ((int(month) == 1 and int(day) >= 20)
         or (int(month) == 2 and int(day) <= 18)):
     sign = ("Aquarius")
 
+# returns Pisces if user input is within month and day parameters
 elif ((int(month) == 2 and int(day) >= 19)
         or (int(month) == 3 and int(day) <= 20)):
     sign = ("Pisces")
+
+# displays Aries if user input is within month and day parameters
 elif ((int(month) == 3 and int(day) >= 21)
         or (int(month) == 4 and int(day) <= 19)):
     sign = ("Aries")
+
+# displays Taurus if user input is within month and day parameters
 elif ((int(month) == 4 and int(day) >= 20)
         or (int(month) == 5 and int(day) <= 20)):
     sign = ("Taurus")
+
+# displays Gemini if user input is within month and day parameters
 elif ((int(month) == 5 and int(day) >= 21)
         or (int(month) == 6 and int(day) <= 20)):
     sign = ("Gemini")
+
+# displays Cancer if user input is within month and day parameters
 elif ((int(month) == 6 and int(day) >= 21)
         or (int(month) == 7 and int(day) <= 22)):
     sign = ("Cancer")
+
+# displays Leo if user input is within month and day parameters
 elif ((int(month) == 7 and int(day) >= 23)
         or (int(month) == 8 and int(day) <= 22)):
     sign = ("Leo")
+
+# displays Virgo if user input is within month and day parameters
 elif ((int(month) == 8 and int(day) >= 23)
         or (int(month) == 9 and int(day) <= 22)):
     sign = ("Virgo")
+
+
+# displays Libra if user input is within month and day parameters
 elif ((int(month) == 9 and int(day) >= 23)
         or (int(month) == 10 and int(day) <= 22)):
     sign = ("Libra")
+
+# displays Scorpio if user input is within month and day parameters
 elif ((int(month) == 10 and int(day) >= 23)
         or (int(month) == 11 and int(day) <= 21)):
     sign = ("Scorpio")
+
+# displays Sagittarius if user input is within month and day parameters
 elif ((int(month) == 11 and int(day) >= 22)
         or (int(month) == 12 and int(day) <= 21)):
     sign = ("Sagittarius")
+
+# displays Capricorn if user input is within month and day parameters
 elif ((int(month) == 12 and int(day) >= 22)
         or (int(month) == 1 and int(day) <= 19)):
     sign = ("Capricorn")
 
+# count down display
 print("Calculating...\n")
 time.sleep(1)
 print("Consulting the cosmos...\n")
